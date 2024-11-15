e9ef5f94cdc4acd3805951010b70326b6d128cc9
lxr <lxr18655043150@163.com>
2024-11-15 09:44:34 +0800

    fix app http server urls log

--- pkg/lib/core/web/http/group.go
+++ pkg/lib/core/web/http/group.go
@@ -31,14 +31,15 @@ func RootRouterGroup() *gin.RouterGroup {
 }
 
 func Run() {
-	if err := appEngine.engine.Run(fmt.Sprintf(":%v", appEngine.port)); err != nil {
-		log.Fatalf("Error starting server: %v", err)
-	}
 
 	urls := make(map[string]string)
 	for _, url := range appEngine.engine.Routes() {
 		urls[url.Method] = url.Path
 	}
-
 	log.Infof("app server http urls:\t%v", urls)
+
+	if err := appEngine.engine.Run(fmt.Sprintf(":%v", appEngine.port)); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+
 }
